118a1754343529c214a80abd35bd0a47dc662466
Jesse Bodell <sruzy.jmb@gmail.com>
2023-09-19 23:22:16 -0600

    added more code: specifically added an if wait on input

--- Propresenter-API-controller-master/Propresenter_API_controller.py
+++ Propresenter-API-controller-master/Propresenter_API_controller.py
@@ -1,20 +1,39 @@
-#dependencies: kivy (2.2.1), requests (2.31.0), virtualenv (20.24.5), pprint
+#dependencies: kivy (2.2.1), requests (2.31.0), virtualenv (20.24.5), keyboard(0.13.5), pprint
 
+#import Setup
+from multiprocessing.connection import wait
 import requests
 import pprint
 import time
-pro_api_url = "http://192.168.0.118:7777"
+import keyboard
+
+
+#Variable Assignment
+####url setup
+local_ip_addr = "http://192.168.0.118"
+local_ip_port = "7777"
+pro_api_url = local_ip_addr+":"+local_ip_port
+
+####api variable setup
 pro_api_looks = pro_api_url+"/v1/looks"
 
-#function
+####function
 response = requests.get(pro_api_looks)
 response.json()
-#print(response.json())
-#time.sleep(1)
 
-with open("looks.txt", "a") as f:
-    read_looks = pprint.pformat(response.json(), width = 1)
-    time.sleep(1)
-    #pprint.isreadable(read_looks)
-    #print(response.json, file=f)
-    f.write(read_looks)
+####pretty conversion
+read_looks = pprint.pformat(response.json(), width = 1)
+
+print("press y to write to file")
+while True:
+    try:
+        if keyboard.is_pressed("y"):
+            print("saving file")
+            with open("looks.txt", "a") as looks_file:
+                looks_file.write(read_looks)
+            break
+        else:
+            break
+    except:
+            break
+
