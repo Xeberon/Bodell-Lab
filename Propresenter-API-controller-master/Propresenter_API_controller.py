fac020a98176ad30d3ba37201b02bb31973cb498
Jesse Bodell <sruzy.jmb@gmail.com>
2023-09-20 00:40:18 -0600

    Temp disabled file save features, begun into gui designing. a bit ambitious? probably...

--- Propresenter-API-controller-master/Propresenter_API_controller.py
+++ Propresenter-API-controller-master/Propresenter_API_controller.py
@@ -1,11 +1,24 @@
 #dependencies: kivy (2.2.1), requests (2.31.0), virtualenv (20.24.5), keyboard(0.13.5), pprint
 
 #import Setup
+import os
 from multiprocessing.connection import wait
+from tkinter import FALSE
 import requests
 import pprint
 import time
 import keyboard
+import kivy
+
+##kivy imports
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+#requirements
+kivy.require("2.2.1")
 
 
 #Variable Assignment
@@ -24,16 +37,49 @@ response.json()
 ####pretty conversion
 read_looks = pprint.pformat(response.json(), width = 1)
 
-print("press y to write to file")
-while True:
-    try:
-        if keyboard.is_pressed("y"):
-            print("saving file")
-            with open("looks.txt", "a") as looks_file:
-                looks_file.write(read_looks)
-            break
-        else:
-            break
-    except:
-            break
+#App Building
 
+class ImportantInfoWidget(GridLayout):
+    pass
+
+class SplashScreen(GridLayout):
+    def __init__(self, **kwargs):
+        super(SplashScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text="IP_ADDR"))
+        self.ipaddr = TextInput(multiline=FALSE)
+        self.add_widget(self.ipaddr)
+        self.add_widget(Label(text="IP_PORT"))
+        self.ipport = TextInput(password=FALSE, multiline=FALSE)
+        self.add_widget(self.ipport)
+
+class SplashConfig(App):
+    def build(self):
+        return SplashScreen()
+
+
+if __name__ == "__main__":
+    SplashConfig().run()
+
+#class Pro_api_exe(App):
+#    def build(self):
+#        return Label(text="testing 1 2 3...")
+
+#if __name__ == "__main__":
+#    Pro_api_exe().run()
+
+
+
+#print("press y to write to file")
+#while True:
+#    try:
+#        if keyboard.is_pressed("y"):
+#            print("saving file")
+#            with open("looks.txt", "a") as looks_file:
+#                looks_file.write(read_looks)
+#            break
+#        else:
+#            break
+#    except:
+#            break
+#
